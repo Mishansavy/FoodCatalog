4d5b5d52e3fba4ef51c49f7546148686a1570354
Mishan Shah <smishan7@gmail.com>
2023-11-21 17:18:19 +0545

    fetching not fix in Foods.jsx

--- src/Components/Category.jsx
+++ src/Components/Category.jsx
@@ -1,18 +1,25 @@
 import { Link } from "react-router-dom";
-
+import client from "../client";
+import imageUrlBuilder from "@sanity/image-url";
+const builder = imageUrlBuilder(client);
+function urlFor(source) {
+  return builder.image(source);
+}
 const Category = ({ category }) => {
   return (
     <div
-      className="bg-gray-100 rounded-xl p-10 w-auto "
+      className="bg-gray-100 rounded-xl p-10 w-80 h-auto "
       style={{ backgroundColor: `#${category.hexCode}` }}
     >
       {category.image && (
         <img
-          src={category.image.asset.url}
+          // src={category.image.asset.url}
+          src={urlFor(category.image).url()}
           alt={category.title}
           className="w-40"
         />
       )}
+      {/* {console.log("first")} */}
       <h4 className="text-2xl py-3 font-bold capitalize">{category.title}</h4>
       <p>{category.description}</p>
       <Link to={"/filteredfoods/" + category.slug.current}>
